9066406cdb89736278a5334a5e0f7aa9593c8cf3
JefreyVIT23 <jefreyjose.d2023@vitstudent.ac.in>
2024-07-27 20:01:52 +0530

    Add files via upload

---
+++ Pgm13.java
@@ -0,0 +1,16 @@
+/*
+Registration Number: 23BAI1326
+Date: 23/7/2024
+Subject: Boolean Functions
+*/
+
+class Pgm13
+{
+public static void main(String[] Args)
+{
+	boolean isjava;
+	boolean isfish = true;
+	System.out.println(isjava);
+	System.out.println(isfish);
+}
+}
